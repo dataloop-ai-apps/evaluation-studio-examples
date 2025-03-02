a93866bec93e9683748a0c72ed8733fc24669a1b
Or Shabtay <or@dataloop.ai>
2025-03-02 15:23:49 +0200

    js update

--- upload.py
+++ upload.py
@@ -7,23 +7,24 @@ with open("examples/index.json", "w") as f:
     examples = [d for d in os.listdir("examples") if os.path.isdir(f"examples/{d}")]
     f.write(json.dumps(examples))
 
-dl.setenv("rc")
-dataset = dl.datasets.get(dataset_id="67b465a6be6c2c1a9e682ee2")
+dl.setenv("prod")
+dataset = dl.datasets.get(dataset_id="67bf09afb4ae7bc93ec8e93b")
 
 binaries_dataset = dataset.project.datasets._get_binaries_dataset()
 
 for folder in os.listdir("examples"):
     if not os.path.isdir(f"examples/{folder}"):
         continue
-    layout_item = binaries_dataset.items.upload(
-        local_path=f"examples/{folder}/form-schema.json",
+    
+    _ = binaries_dataset.items.upload(
+        local_path=f"examples/{folder}/*",
         remote_path=f"/.dataloop/evaluation-studio-layouts",
-        remote_name=f"{folder}.json",
         overwrite=True,
     )
+    
 
     data_item = dataset.items.upload(
-        local_path=f"examples/{folder}/item.json",
+        local_path=f"examples/{folder}/{folder}-data.json",
         remote_name=f"{folder}-example.json",
         overwrite=True,
         item_metadata={
